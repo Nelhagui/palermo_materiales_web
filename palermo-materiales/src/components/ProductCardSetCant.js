ef6ad80606c45642418c9f9e6506764930e5f833
EXB00614 <matias.maruzza@claro.com.ar>
2022-12-07 14:49:40 -0300

    fixed productCardSetCant cont

--- palermo-materiales/src/components/ProductCardSetCant.js
+++ palermo-materiales/src/components/ProductCardSetCant.js
@@ -1,92 +1,96 @@
 import { useState, useContext, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import CartContext from '../context/CartContext.js'
 
 const ProductCardSetCant = () => {
-    const [product, setProduct] = useState(null);
-    const [productStorege] = useState(JSON.parse(localStorage.getItem("producto-agregar")));
+  const [product, setProduct] = useState(null)
+  const [productStorege] = useState(
+    JSON.parse(localStorage.getItem('producto-agregar')),
+  )
+  const [cont, setCont] = useState(0)
 
-    let { id } = useParams()
+  let { id } = useParams()
+  const handleRest = () => {
+    if (cont > 0) {
+        setCont(cont - 1)
+    }
+  }
 
-    function fetchProducto() {
-        if (id) {
-          axios
-            .get(
-              `https://api.palermomateriales.com.ar/api/productocombinado/${id}`,
-            )
-            .then((res) => {
-                setProduct(res?.data)
-            })
-        }
-      }
+  const handleAdd = () => {
+    setCont(cont + 1)
+  }
 
-    useEffect(() => {
-        if(productStorege !== null)
-        {
-            if(productStorege.id == id){
-                setProduct(productStorege);
-            }
-            else
-                fetchProducto();
+  function fetchProducto() {
+    if (id) {
+      axios
+        .get(`https://api.palermomateriales.com.ar/api/productocombinado/${id}`)
+        .then((res) => {
+          setProduct(res?.data)
+        })
+    }
+  }
 
-        } else {
-            fetchProducto()
-        }
-    }, [productStorege])
-    
-    console.warn(productStorege)
-  
+  useEffect(() => {
+    if (productStorege !== null) {
+      if (productStorege.id == id) {
+        setProduct(productStorege)
+      } else fetchProducto()
+    } else {
+      fetchProducto()
+    }
+  }, [productStorege])
+
+  console.warn(productStorege)
+
   return (
     <div className="container-history-set-product">
-        <div className="history-set-product">
-            <div>este es el historial -  este es el historial</div>
-            <div className="cont-gral-set-product">
-                <div className="img-info row">
-                    <div className="cont-img col-12 col-md-4">
-                        <img src={product?.foto} alt="" />
-                    </div>
-                    <div className="info-set-cant col-12 col-md-8">
-                        <div className="title-price">
-                            <h1>{product?.productos_simples[0].alias}</h1>
-                            <p>${product?.productos_simples[0].precio_x_unidad}</p>
-                        </div>
-                        <div className="cont-caract-prod">
-                            <div>
-                                <p>Marca:{product?.marca}</p>
-                                <p>Peso: {product?.peso}</p>
-                            </div>
-                            <div>
-                                <p>Tipo:{product?.tipo}</p>
-                                <p>Volumen: {product?.volumen}</p>
-                            </div>
-                        </div>
-                        <div className="cont-contador-buttom row">
-                            <div className="col-12 col-md-6">
-                                <div className="contador">
-                                    <div className="" >
-                                    -
-                                    </div>
-                                    <div>1</div>
-                                    <div className="" >
-                                    +
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <button className="btn-primary">
-                                    COTIZAR
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="details-product row">
-                    <h3>Detalle</h3>
-                    <p>{product?.descripcion_larga}</p>
-                </div>
+      <div className="history-set-product">
+        <div>este es el historial - este es el historial</div>
+        <div className="cont-gral-set-product">
+          <div className="img-info row">
+            <div className="cont-img col-12 col-md-4">
+              <img src={product?.foto} alt="" />
             </div>
+            <div className="info-set-cant col-12 col-md-8">
+              <div className="title-price">
+                <h1>{product?.productos_simples[0]?.alias}</h1>
+                <p>${product?.productos_simples[0]?.precio_x_unidad}</p>
+              </div>
+              <div className="cont-caract-prod">
+                <div>
+                  <p>Marca:{product?.marca}</p>
+                  <p>Peso: {product?.peso}</p>
+                </div>
+                <div>
+                  <p>Tipo:{product?.tipo}</p>
+                  <p>Volumen: {product?.volumen}</p>
+                </div>
+              </div>
+              <div className="cont-contador-buttom row">
+                <div className="col-12 col-md-6">
+                  <div className="contador">
+                    <div className="px-5" onClick={() => handleRest()}>
+                      -
+                    </div>
+                    <div>{cont}</div>
+                    <div className="px-5" onClick={() => handleAdd()}>
+                      +
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12 col-md-6">
+                  <button className="btn-primary">COTIZAR</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="details-product row">
+            <h3>Detalle</h3>
+            <p>{product?.descripcion_larga}</p>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
